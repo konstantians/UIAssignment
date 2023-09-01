624c30be76da1f579b5d87c865d9fbb880b716b7
iliasLgt <ililig2000@gmail.com>
2023-09-01 21:03:17 +0300

    Removed unnecessary buttons and logo from the main menu and added on-line help

--- UIAssignment/Forms/CommonForms/MainForm.Designer.cs
+++ UIAssignment/Forms/CommonForms/MainForm.Designer.cs
@@ -29,126 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.cool_button4 = new UIAssignment.Resources.Cool_button();
-            this.cool_button3 = new UIAssignment.Resources.Cool_button();
-            this.cool_button1 = new UIAssignment.Resources.Cool_button();
-            this.cool_button2 = new UIAssignment.Resources.Cool_button();
-            this.logoPictureBox = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // cool_button4
-            // 
-            this.cool_button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cool_button4.BackColor = System.Drawing.SystemColors.Control;
-            this.cool_button4.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.cool_button4.BackgroundImage = global::UIAssignment.Properties.Resources.PoolImage;
-            this.cool_button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.cool_button4.BorderColor = System.Drawing.Color.Black;
-            this.cool_button4.BorderRadius = 40;
-            this.cool_button4.BorderSize = 3;
-            this.cool_button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cool_button4.FlatAppearance.BorderSize = 0;
-            this.cool_button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cool_button4.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.cool_button4.ForeColor = System.Drawing.Color.Black;
-            this.cool_button4.Location = new System.Drawing.Point(697, 591);
-            this.cool_button4.Name = "cool_button4";
-            this.cool_button4.Padding = new System.Windows.Forms.Padding(0, 4, 0, 0);
-            this.cool_button4.Size = new System.Drawing.Size(260, 135);
-            this.cool_button4.TabIndex = 20;
-            this.cool_button4.Text = "Πισίνα";
-            this.cool_button4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.cool_button4.TextColor = System.Drawing.Color.Black;
-            this.cool_button4.UseVisualStyleBackColor = false;
-            // 
-            // cool_button3
-            // 
-            this.cool_button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cool_button3.BackColor = System.Drawing.SystemColors.ControlText;
-            this.cool_button3.BackgroundColor = System.Drawing.SystemColors.ControlText;
-            this.cool_button3.BackgroundImage = global::UIAssignment.Properties.Resources.TrojanHorseImage;
-            this.cool_button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.cool_button3.BorderColor = System.Drawing.Color.White;
-            this.cool_button3.BorderRadius = 40;
-            this.cool_button3.BorderSize = 3;
-            this.cool_button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cool_button3.FlatAppearance.BorderSize = 0;
-            this.cool_button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cool_button3.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.cool_button3.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.cool_button3.Location = new System.Drawing.Point(1002, 591);
-            this.cool_button3.Name = "cool_button3";
-            this.cool_button3.Padding = new System.Windows.Forms.Padding(0, 4, 0, 0);
-            this.cool_button3.Size = new System.Drawing.Size(260, 135);
-            this.cool_button3.TabIndex = 19;
-            this.cool_button3.Text = "Δούρειος Ίππος";
-            this.cool_button3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.cool_button3.TextColor = System.Drawing.Color.WhiteSmoke;
-            this.cool_button3.UseVisualStyleBackColor = false;
-            // 
-            // cool_button1
-            // 
-            this.cool_button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cool_button1.BackColor = System.Drawing.SystemColors.Control;
-            this.cool_button1.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.cool_button1.BackgroundImage = global::UIAssignment.Properties.Resources.ApartmentImage;
-            this.cool_button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.cool_button1.BorderColor = System.Drawing.Color.Black;
-            this.cool_button1.BorderRadius = 40;
-            this.cool_button1.BorderSize = 3;
-            this.cool_button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cool_button1.FlatAppearance.BorderSize = 0;
-            this.cool_button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cool_button1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.cool_button1.ForeColor = System.Drawing.Color.Black;
-            this.cool_button1.Location = new System.Drawing.Point(697, 402);
-            this.cool_button1.Name = "cool_button1";
-            this.cool_button1.Padding = new System.Windows.Forms.Padding(0, 4, 0, 0);
-            this.cool_button1.Size = new System.Drawing.Size(260, 135);
-            this.cool_button1.TabIndex = 18;
-            this.cool_button1.Text = "Διαμέρισμα";
-            this.cool_button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.cool_button1.TextColor = System.Drawing.Color.Black;
-            this.cool_button1.UseVisualStyleBackColor = false;
-            // 
-            // cool_button2
-            // 
-            this.cool_button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cool_button2.BackColor = System.Drawing.SystemColors.Control;
-            this.cool_button2.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.cool_button2.BackgroundImage = global::UIAssignment.Properties.Resources.RestaurantImage;
-            this.cool_button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.cool_button2.BorderColor = System.Drawing.Color.Black;
-            this.cool_button2.BorderRadius = 40;
-            this.cool_button2.BorderSize = 3;
-            this.cool_button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.cool_button2.FlatAppearance.BorderSize = 0;
-            this.cool_button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cool_button2.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.cool_button2.ForeColor = System.Drawing.Color.Black;
-            this.cool_button2.Location = new System.Drawing.Point(1002, 402);
-            this.cool_button2.Name = "cool_button2";
-            this.cool_button2.Padding = new System.Windows.Forms.Padding(0, 4, 0, 0);
-            this.cool_button2.Size = new System.Drawing.Size(260, 135);
-            this.cool_button2.TabIndex = 17;
-            this.cool_button2.Text = "Εστιατόριο";
-            this.cool_button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.cool_button2.TextColor = System.Drawing.Color.Black;
-            this.cool_button2.UseVisualStyleBackColor = false;
-            // 
-            // logoPictureBox
-            // 
-            this.logoPictureBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.logoPictureBox.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.logoPictureBox.BackgroundImage = global::UIAssignment.Properties.Resources.TranspareFullLogo;
-            this.logoPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.logoPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.logoPictureBox.Location = new System.Drawing.Point(456, 12);
-            this.logoPictureBox.Name = "logoPictureBox";
-            this.logoPictureBox.Size = new System.Drawing.Size(343, 135);
-            this.logoPictureBox.TabIndex = 21;
-            this.logoPictureBox.TabStop = false;
             // 
             // MainForm
             // 
@@ -158,25 +39,13 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1310, 750);
-            this.Controls.Add(this.logoPictureBox);
-            this.Controls.Add(this.cool_button4);
-            this.Controls.Add(this.cool_button3);
-            this.Controls.Add(this.cool_button1);
-            this.Controls.Add(this.cool_button2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.Text = "Menu";
-            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Resources.Cool_button cool_button3;
-        private Resources.Cool_button cool_button1;
-        private Resources.Cool_button cool_button2;
-        private Resources.Cool_button cool_button4;
-        private System.Windows.Forms.PictureBox logoPictureBox;
     }
 }
